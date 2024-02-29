cef04fa1816dc6f5532349c0c3badc3e2301d89a
crym22 <ahmetefe.saritas@ademceylantk.com.tr>
2024-02-29 11:04:07 +0300

    kralahmet

---
+++ main.dart
@@ -0,0 +1,7 @@
+import 'constuructor.dart';
+
+void main() {
+  Calisan c1=new Calisan("ahmet", "sarıtaş",Departman.Yazilimci);
+  Calisan c2=new Calisan("ahmet", "sarıtaş",Departman.Pazarlamaci);
+  
+  }
